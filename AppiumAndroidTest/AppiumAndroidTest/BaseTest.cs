20fbb78d3e5f188c4d051de6e83be2ade957aa9e
Marmadyk <95248327+Marmadyk@users.noreply.github.com>
2022-02-02 21:00:20 +0200

    Home Work
    Pls follow steps in README file to run the test

---
+++ AppiumAndroidTest/AppiumAndroidTest/BaseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace AppiumAndroidTest
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        protected AppiumDriver<AndroidElement> _driver;
+
+        [SetUp]
+        protected void DoBeforeEach()
+        {
+            var driverOption = new AppiumOptions();
+            driverOption.AddAdditionalCapability("platformName", "Android");
+            driverOption.AddAdditionalCapability("deviceName", "emulator-5554");
+            driverOption.AddAdditionalCapability("appActivity", "com.google.android.gm.GmailActivity");
+            driverOption.AddAdditionalCapability("appPackage", "com.google.android.gm");
+            driverOption.AddAdditionalCapability("chromedriverExecutable", @"/Users/applecity/Desktop/chromedriver");
+            _driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+        }
+
+        [TearDown]
+        protected void DoAfterEach()
+        {
+            _driver.Quit();
+        }
+    }
+}
